27501413719b2bca69742ed97308158d88e72779
rusuioana7 <rusuioana37@gmail.com>
2023-06-22 16:44:02 +0300

    statistics miscroservice + admin front

---
+++ service/statistics/database.js
@@ -0,0 +1,25 @@
+const mysql =require("mysql");
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "STUDENT",
+    database: "mydb"
+});
+
+con.connect(function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("Connected to database!");
+
+});
+
+ function getDb () {
+    if (con) {
+        return con;
+    } else {
+        throw 'No database found!'
+    }
+}
+module.exports={getDb}
